81cb6bba474be8270a81b1699a425c10b57fe7f3
ainatte-inbal <ainatte-inbal@users.noreply.github.com>
2026-01-27 13:05:03 +0200

    Make Slack username a clickable link

--- components/FeedbackButton.tsx
+++ components/FeedbackButton.tsx
@@ -5,7 +5,12 @@ export default function FeedbackButton() {
     <div className="fixed bottom-4 right-4 z-50">
       <div className="bg-white rounded-full shadow-lg border border-gray-200 px-4 py-2 text-sm text-gray-600">
         💬 Want to share your feedback? DM{" "}
-        <span className="font-semibold text-intuit-blue">@ainatte inbal</span>{" "}
+        <a
+          href="slack://user?team=T024FJS4L&id=U02PHPRFQM6"
+          className="font-semibold text-intuit-blue hover:underline"
+        >
+          @ainatte inbal
+        </a>{" "}
         on Slack!
       </div>
     </div>
